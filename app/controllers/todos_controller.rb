00ac7fff03f3d79f147a03780aa64bf401fc5264
ChowtooriKedari <chowtoorikedari@gmail.com>
2020-04-08 10:40:35 +0530

    Formatted Codes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,5 @@
 class TodosController < ApplicationController
-  #skip_before_action :verify_authenticity_token
-
   def index
-    # render plain: Todo.order(:id).map { |todo| todo.to_pleasant_string }.join("\n")
     render "index"
   end
 
@@ -10,7 +7,6 @@ class TodosController < ApplicationController
     id = params[:id]
     todo = Todo.find(id)
     render "todo"
-    #render plain: todo.to_pleasant_string
   end
 
   def create
