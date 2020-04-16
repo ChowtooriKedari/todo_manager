fb48b3b25694cde1318ef9b86366baf40e205032
ChowtooriKedari <chowtoorikedari@gmail.com>
2020-04-16 22:54:21 +0530

    Todos and Users Verification

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,24 +1,25 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.of_user(current_user)
     render "index"
   end
 
   def show
     id = params[:id]
-    todo = Todo.find(id)
+    todo = Todo.of_user(current_user).find(id)
     render "todo"
   end
 
   def create
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
-    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false, user_id: current_user.id)
     redirect_to todos_path
   end
 
   def destroy
     id = params[:id]
-    todo = Todo.find(id)
+    todo = Todo.of_user(current_user).find(id)
     todo.destroy
     redirect_to todos_path
   end
@@ -26,7 +27,7 @@ class TodosController < ApplicationController
   def update
     id = params[:id]
     completed = params[:completed]
-    todo = Todo.find(id)
+    todo = Todo.of_user(current_user).find(id)
     todo.completed = completed
     todo.save!
     redirect_to todos_path
