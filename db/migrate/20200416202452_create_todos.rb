d3942a273a3ca27c26121993914381c32d804569
ChowtooriKedari <chowtoorikedari@gmail.com>
2020-04-17 02:06:59 +0530

    Validations and Flash Messages

---
+++ db/migrate/20200416202452_create_todos.rb
@@ -0,0 +1,10 @@
+class CreateTodos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :todos do |t|
+      t.text :todo_text
+      t.date :due_date
+      t.boolean :completed
+      t.bigint :user_id
+    end
+  end
+end
