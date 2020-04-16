7d363c34ee7c4c2ef5e3c0de52b8e7662db9a2c4
ChowtooriKedari <chowtoorikedari@gmail.com>
2020-04-17 02:53:28 +0530

    Validations and Flash Messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_120622) do
+ActiveRecord::Schema.define(version: 2020_04_16_202452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_04_15_120622) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
